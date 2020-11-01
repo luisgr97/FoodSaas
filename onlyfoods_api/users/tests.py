85900dd34b3fdb3e578e9403ceea3b177b84d184
Camilo Sanchez <juan.barreiro@correounivalle.edu.co>
2020-11-01 15:39:05 -0500

    fixed bugs in /users/

--- onlyfoods_api/users/tests.py
+++ onlyfoods_api/users/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import TypeCustomUser
+from django_tenants.utils import schema_context
+
+with schema_context('kokoriko'):
+    TypeCustomUser.objects.create(typeusers="superusuario")
+    TypeCustomUser.objects.create(typeusers="administrador")
+    TypeCustomUser.objects.create(typeusers="digitador")
+    TypeCustomUser.objects.create(typeusers="cliente")
 
 # Create your tests here.
